ffa47b7684ec24717dd2dd9925ecdd42a5164b1b
Pankaj Bhatt <pankaj.x.bhatt@tdsecurities.com>
2018-05-31 15:53:26 -0400

    Added Cloud Server details.

---
+++ Cloud_Zuul_Server/src/main/java/com/pankaj/cloud/eureka/CloudApplication.java
@@ -0,0 +1,16 @@
+package com.pankaj.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+}
